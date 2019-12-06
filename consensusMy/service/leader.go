3cee0692536102c8c86c75c29007d4496c47635c
ZZY <iezhuzhouyi@163.com>
2019-12-06 13:03:46 +0800

    keep

--- consensusMy/service/leader.go
+++ consensusMy/service/leader.go
@@ -1,6 +1,14 @@
 package service
 
-import "encoding/json"
+import (
+	"BCDns_0.1/blockChain"
+	"BCDns_0.1/messages"
+	"BCDns_0.1/network/service"
+	"encoding/json"
+	"fmt"
+	"reflect"
+	"time"
+)
 
 var (
 	LeaderRole *LeaderT
@@ -8,9 +16,23 @@ var (
 
 type LeaderT struct {
 	ProposalMessageChan chan []byte
+	MessagePool ProposalMessagePool
 }
 
-func (l *LeaderT) Run() {
+func (l *LeaderT) Run(done chan uint) {
+	defer close(done)
+	interrupt := make(chan int)
+	interruptTimer := make(chan int)
+	go func() {
+		for true {
+			select {
+			case <-time.After(10 * time.Second):
+				interrupt <- 1
+			case <-interruptTimer:
+				interrupt <- 1
+			}
+		}
+	}()
 	for {
 		select {
 		case msgByte := <-l.ProposalMessageChan:
@@ -20,28 +42,81 @@ func (l *LeaderT) Run() {
 				logger.Warningf("[Leader.Run] json.Unmarshal error=%v", err)
 				continue
 			}
-			l.handleProposal(msg)
+			switch msg.Type {
+			case Add:
+				if !msg.ValidateAdd() {
+					logger.Warningf("[Leader.Run] ValidateAdd failed")
+					continue
+				}
+			case Del:
+				if !msg.ValidateDel() {
+					logger.Warningf("[Leader.Run] ValidateDel failed")
+					continue
+				}
+			case Mod:
+				if !msg.ValidateMod() {
+					logger.Warningf("[Leader.Run] ValidateMod failed")
+					continue
+				}
+			}
+			l.MessagePool.AddProposal(msg)
+			if l.MessagePool.Size() >= blockChain.BlockMaxSize {
+				interruptTimer <- 1
+			}
+		case <- interrupt:
+			if !service.Leader.IsLeader() {
+				continue
+			}
+			if l.MessagePool.Size() <= 0 {
+				fmt.Printf("[Leader.Run] CurrentBlock is empty\n")
+				continue
+			}
+			validP, abandonedP := CheckProposal(l.MessagePool)
 		}
 	}
 }
 
-func (l *LeaderT) handleProposal(proposal ProposalMessage) {
-	switch proposal.Type {
-	case Add:
-		if !proposal.ValidateAdd() {
-			logger.Warningf("[handleProposal] ValidateAdd failed")
-			return
-		}
-	case Del:
-		if !proposal.ValidateDel() {
-			logger.Warningf("[handleProposal] ValidateDel failed")
-			return
-		}
-	case Mod:
-		if !proposal.ValidateMod() {
-			logger.Warningf("[handleProposal] ValidateMod failed")
-			return
+func CheckProposal(proposals ProposalMessagePool) (ProposalMessages, ProposalMessages) {
+	filter := make(map[string]ProposalMessages)
+	abandoneP := ProposalMessagePool{}
+	validP := ProposalMessagePool{}
+	for _, p := range proposals.ProposalMessages {
+		if fp, ok := filter[p.ZoneName]; !ok {
+			filter[p.ZoneName] = append(filter[p.ZoneName], p)
+			validP.AddProposal(p)
+		} else {
+			drop := false
+			for _, tmpP := range filter[p.ZoneName] {
+				if reflect.DeepEqual(p.Id, tmpP.Id) {
+					drop = true
+					break
+				}
+			}
+			if !drop {
+				//TODO: Two conflicted proposal
+				tmpP := fp[len(fp)-1]
+				switch p.Type {
+				case Add:
+					if tmpP.Owner != messages.Dereliction {
+						abandoneP.AddProposal(p)
+					} else {
+						validP.AddProposal(p)
+					}
+				case Mod:
+					if tmpP.Owner != p.Owner || tmpP.Owner != p.From {
+						abandoneP.AddProposal(p)
+					} else {
+						validP.AddProposal(p)
+					}
+				case Del:
+					if p.Owner != messages.Dereliction || tmpP.Owner != p.From {
+						abandoneP.AddProposal(p)
+					} else {
+						validP.AddProposal(p)
+					}
+				}
+			}
 		}
 	}
-
+	return validP.ProposalMessages, abandoneP.ProposalMessages
 }
