51fcea85db27ed27a0895f39f509557885d4073c
ZZY <iezhuzhouyi@163.com>
2020-01-07 18:17:58 +0800

    add generateCert script through multi node

--- generateCertDistribute.sh
+++ generateCertDistribute.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /go/src/BCDns_0.1
+cd /go/src/BCDns_0.1/certificateAuthority/conf
 
 cat /tmp/hosts | while read line
 do
@@ -8,7 +8,7 @@ do
     hostname=$(echo $line | awk '{print $2}')
     ./generateCertByIp.sh $ip CH BJ BJ BUPT 222 $hostname
     expect -c "
-        spawn scp ./certificateAuthority/conf/tmp/$hostname.cer root@$ip:/go/src/BCDns_0.1/certificateAuthority/conf/$hostname/LocalCertificate.cer
+        spawn scp ./tmp/$hostname.cer root@$ip:/go/src/BCDns_0.1/certificateAuthority/conf/$hostname/LocalCertificate.cer
 	    expect {
             \"*yes/no*\" {send \"yes\r\";exp_continue;}
             \"*assword\" {set timeout 300; send \"123456\r\"; exp_continue;}
